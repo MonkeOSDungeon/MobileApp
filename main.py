d6c4d5d786313693c71b4015edbe08d69a4ca515
Ilya <122489486+IlyaAbramkin@users.noreply.github.com>
2024-05-20 23:25:37 +0400

    Сервер
    Добавлена возможность подключать две камеры. Не было возможности протестировать. Прием сигнала с камер и отправка клиенту скорее всего работают. Десктопное приложение скорее всего поломалось.

--- main.py
+++ main.py
@@ -19,143 +19,167 @@ import time
 
 
 class VideoThread(QThread):
-    change_pixmap_signal = Signal(np.ndarray)
+    change_pixmap_signal_1 = Signal(np.ndarray)
+    change_pixmap_signal_2 = Signal(np.ndarray)
 
-    def __init__(self, detector: Detector, cap, is_active_detector: bool, server_ip='192.168.0.105', server_port=8000):
+    def __init__(self, detector_1: Detector, detector_2: Detector, cap_1, cap_2, is_active_detector_1: bool, is_active_detector_2: bool, server_ip='192.168.0.105', server_port_1=8000, server_port_2=8001):
         super().__init__()
-        self.detector = detector
-        self.cap = cap
-        self.is_active_detector = is_active_detector
+        self.detector_1 = detector_1
+        self.detector_2 = detector_2
+        self.cap_1 = cap_1
+        self.cap_2 = cap_2
+        self.is_active_detector_1 = is_active_detector_1
+        self.is_active_detector_2 = is_active_detector_2
         self.server_ip = server_ip
-        self.server_port = server_port
-        self.reciever_email = None
-        self.sender = None
-        self.server_socket = None
-        self.client_socket = None
-        self.client_connected = False
+        self.server_port_1 = server_port_1
+        self.server_port_2 = server_port_2
+        self.client_socket_1 = None
+        self.client_socket_2 = None
+        self.server_socket_1 = None
+        self.server_socket_2 = None
+        self.client_connected_1 = False
+        self.client_connected_2 = False
 
-    def set_active_detector(self, is_active: bool) -> None:
-        '''
-        Set is_active_detector to is_active value
-        '''
-        self.is_active_detector = is_active
+    def set_active_detector_1(self, is_active: bool) -> None:
+        self.is_active_detector_1 = is_active
+
+    def set_active_detector_2(self, is_active: bool) -> None:
+        self.is_active_detector_2 = is_active
 
     def run(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_socket.bind((self.server_ip, self.server_port))
-        self.server_socket.listen(5)
-        self.server_socket.settimeout(1)  # Set timeout for accept()
+        self.server_socket_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket_1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket_1.bind((self.server_ip, self.server_port_1))
+        self.server_socket_1.listen(5)
+        self.server_socket_1.settimeout(1)
 
-        print(f"Listening for connection on {self.server_ip}:{self.server_port}")
+        self.server_socket_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket_2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket_2.bind((self.server_ip, self.server_port_2))
+        self.server_socket_2.listen(5)
+        self.server_socket_2.settimeout(1)
 
-        last_send = 0
+        print(f"Listening for connections on {self.server_ip}:{self.server_port_1} and {self.server_ip}:{self.server_port_2}")
+
         while True:
+            # Accept connections for the first stream
             try:
-                self.client_socket, _ = self.server_socket.accept()
-                self.client_connected = True
-                print("Client connected")
+                self.client_socket_1, _ = self.server_socket_1.accept()
+                self.client_connected_1 = True
+                print("Client 1 connected")
             except socket.timeout:
-                pass  # No client connected within the timeout period
+                pass
 
-            _, frame = self.cap.read()
-            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-            if not self.is_active_detector:
-                frame = cv2.resize(frame, (1280, 720))
+            # Accept connections for the second stream
+            try:
+                self.client_socket_2, _ = self.server_socket_2.accept()
+                self.client_connected_2 = True
+                print("Client 2 connected")
+            except socket.timeout:
+                pass
+
+            # Read and process frame from the first stream
+            _, frame_1 = self.cap_1.read()
+            frame_1 = cv2.rotate(frame_1, cv2.ROTATE_90_CLOCKWISE)
+            if not self.is_active_detector_1:
+                frame_1 = cv2.resize(frame_1, (1280, 720))
             else:
-                was_human_detected_in_zone, frame = self.detector.detect(frame)
-                # if was_human_detected_in_zone:
-                    # curr_time = time.time()
-                    # if curr_time - last_send > 60 * 5 and
-                    # add sending email and push notification to mobile app
-            # print(self.is_active_detector)
-            self.change_pixmap_signal.emit(frame)
+                _, frame_1 = self.detector_1.detect(frame_1)
 
-            if self.client_connected:
+            self.change_pixmap_signal_1.emit(frame_1)
+
+            # Send the first stream to the client if connected
+            if self.client_connected_1:
                 try:
-                    self.send_frame(frame)
+                    self.send_frame(self.client_socket_1, frame_1)
                 except (BrokenPipeError, ConnectionResetError):
-                    self.client_socket.close()
-                    self.client_connected = False
-                    print("Client disconnected")
+                    self.client_socket_1.close()
+                    self.client_connected_1 = False
+                    print("Client 1 disconnected")
 
-    def send_frame(self, frame):
-        _, buffer = cv2.imencode('.jpg', frame)
-        data = buffer.tobytes()
-        size = len(data)
-        self.client_socket.sendall(struct.pack(">L", size) + data)
+            # Read and process frame from the second stream
+                    _, frame_2 = self.cap_2.read()
+                    frame_2 = cv2.rotate(frame_2, cv2.ROTATE_90_CLOCKWISE)
+                    if not self.is_active_detector_2:
+                        frame_2 = cv2.resize(frame_2, (1280, 720))
+                    else:
+                        _, frame_2 = self.detector_2.detect(frame_2)
 
+                    self.change_pixmap_signal_2.emit(frame_2)
+
+                    # Send the second stream to the client if connected
+                    if self.client_connected_2:
+                        try:
+                            self.send_frame(self.client_socket_2, frame_2)
+                        except (BrokenPipeError, ConnectionResetError):
+                            self.client_socket_2.close()
+                            self.client_connected_2 = False
+                            print("Client 2 disconnected")
+
+            def send_frame(self, client_socket, frame):
+                _, buffer = cv2.imencode('.jpg', frame)
+                data = buffer.tobytes()
+                size = len(data)
+                client_socket.sendall(struct.pack(">L", size) + data)
 
 class HumanDetectorDesktopApp(QMainWindow):
     def __init__(self) -> None:
-        '''
-        args:
-                video_stream_path: path to video stream, default = 0 (video stream from web camera)
-        '''
         super(HumanDetectorDesktopApp, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.detector = Detector(resolution=(1280, 720), zone=np.array([1000, 0, 500, 0, 500, 720, 1000, 720], dtype=int).reshape((4, 2)))
-        # Получаем видео через UDP
-        self.cap = cv2.VideoCapture('udp://192.168.0.105:40002', cv2.CAP_FFMPEG)
+        # Initialize two detectors
+        self.detector_1 = Detector(
+            resolution=(1280, 720),
+            zone=np.array([1000, 0, 500, 0, 500, 720, 1000, 720], dtype=int).reshape((4, 2))
+        )
+        self.detector_2 = Detector(
+            resolution=(1280, 720),
+            zone=np.array([1000, 0, 500, 0, 500, 720, 1000, 720], dtype=int).reshape((4, 2))
+        )
 
-        # Ожидание получения видеопотока
-        while not self.cap.isOpened():
-            self.cap = cv2.VideoCapture('udp://192.168.0.105:40002', cv2.CAP_FFMPEG)
-            print("Ожидание получения видеопотока...")
-            time.sleep(3)  # Задержка в 5 секунд
+        # Initialize two video captures via UDP
+        self.cap_1 = cv2.VideoCapture('udp://192.168.0.105:40002', cv2.CAP_FFMPEG)
+        self.cap_2 = cv2.VideoCapture('udp://192.168.0.105:40003', cv2.CAP_FFMPEG)
 
-        print("Видеопоток получен!")
+        # Waiting for the first video stream
+        while not self.cap_1.isOpened():
+            self.cap_1 = cv2.VideoCapture('udp://192.168.0.105:40002', cv2.CAP_FFMPEG)
+            print("Waiting for video stream 1...")
+            time.sleep(3)
+
+        print("Video stream 1 received!")
+
+        # Waiting for the second video stream
+        while not self.cap_2.isOpened():
+            self.cap_2 = cv2.VideoCapture('udp://192.168.0.105:40003', cv2.CAP_FFMPEG)
+            print("Waiting for video stream 2...")
+            time.sleep(3)
+
+        print("Video stream 2 received!")
 
         self.sender_email = None
 
         self.ui.settings.triggered.connect(self.open_settings_window)
 
-        self.video_stream = self.ui.video_stream
-        self.is_active_detector = True
+        self.video_stream_1 = self.ui.video_stream_1
+        self.video_stream_2 = self.ui.video_stream_2
 
-        self.ui.activate_people_detector.clicked.connect(self.activate_detector_button_clicked)
+        self.is_active_detector_1 = True
+        self.is_active_detector_2 = True
 
-        self.thread = VideoThread(self.detector, self.cap, self.is_active_detector)
-        self.thread.change_pixmap_signal.connect(self.update_image)
+        self.ui.activate_people_detector_1.clicked.connect(self.activate_detector_button_1_clicked)
+        self.ui.activate_people_detector_2.clicked.connect(self.activate_detector_button_2_clicked)
+
+        self.thread = VideoThread(
+            self.detector_1, self.detector_2,
+            self.cap_1, self.cap_2,
+            self.is_active_detector_1, self.is_active_detector_2
+        )
+
+        self.thread.change_pixmap_signal_1.connect(self.update_image_1)
+        self.thread.change_pixmap_signal_2.connect(self.update_image_2)
         self.thread.start()
-
-    def log_in(self):
-        '''
-        connects to email server
-
-        return: touple(sender email, smtp server)
-        '''
-        server = smtplib.SMTP_SSL("smtp.gmail.com",465)
-        server.login(self.sender_email, self.sender_pass)# login to email
-        return self.sender_email, server
-
-    def send_email(self, to_email: str, image_path: str, from_email: str, server: smtplib.SMTP_SSL) -> None:
-        '''
-        sends email with text and image
-
-        args:
-            to email: reciever email
-            image_path: path to image
-            from email: sender email
-            server: smtp server
-        return:
-            none
-        '''
-        message = MIMEMultipart()
-        message['From'] = from_email
-        message['To'] = to_email
-        message['Subject'] = ""
-        # Add in the message body
-        message_body = ""
-        # add image
-        message.attach(MIMEText(message_body, 'plain'))
-        with open(image_path, 'rb') as file:
-            image = MIMEImage(file.read())
-            image.add_header('Content-Disposition', 'attachment', filename='image.jpg')
-            message.attach(image)
-        server.sendmail(from_email, to_email, message.as_string())
 
     def open_settings_window(self):
         self.new_window = QtWidgets.QDialog()
@@ -184,22 +208,38 @@ class HumanDetectorDesktopApp(QMainWindow):
         cords.extend(list(map(int, self.ui_settings_window.le_left_top_cords.text().split())))
         cords.extend(list(map(int, self.ui_settings_window.le_left_bottom_cords.text().split())))
         cords.extend(list(map(int, self.ui_settings_window.le_right_bottom_cords.text().split())))
-        self.detector.change_zone(np.array(cords, dtype=int).reshape((4, 2)))
+        self.detector_1.change_zone(np.array(cords, dtype=int).reshape((4, 2)))
+        self.detector_2.change_zone(np.array(cords, dtype=int).reshape((4, 2)))
 
-    def activate_detector_button_clicked(self):
-        if self.is_active_detector:
-            self.is_active_detector = False
-            self.ui.activate_people_detector.setText('Выключить\nраспознавание людей\nна видео')
+    def activate_detector_button_1_clicked(self):
+        if self.is_active_detector_1:
+            self.is_active_detector_1 = False
+            self.ui.activate_people_detector_1.setText('Выключить\nраспознавание людей\nна видео')
         else:
-            self.is_active_detector = True
-            self.ui.activate_people_detector.setText('Включить\nраспознавание людей\nна видео')
+            self.is_active_detector_1 = True
+            self.ui.activate_people_detector_1.setText('Включить\nраспознавание людей\nна видео')
 
-        self.thread.set_active_detector(self.is_active_detector)
+        self.thread.set_active_detector_1(self.is_active_detector_1)
+
+    def activate_detector_button_2_clicked(self):
+        if self.is_active_detector_2:
+            self.is_active_detector_2 = False
+            self.ui.activate_people_detector_2.setText('Выключить\nраспознавание людей\nна видео')
+        else:
+            self.is_active_detector_2 = True
+            self.ui.activate_people_detector_2.setText('Включить\nраспознавание людей\nна видео')
+
+        self.thread.set_active_detector_2(self.is_active_detector_2)
 
     @Slot(np.ndarray)
-    def update_image(self, cv_img):
+    def update_image_1(self, cv_img):
         qt_img = self.convert_cv_qt(cv_img)
-        self.video_stream.setPixmap(qt_img)
+        self.video_stream_1.setPixmap(qt_img)
+
+    @Slot(np.ndarray)
+    def update_image_2(self, cv_img):
+        qt_img = self.convert_cv_qt(cv_img)
+        self.video_stream_2.setPixmap(qt_img)
 
     def convert_cv_qt(self, cv_img):
         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
@@ -208,10 +248,6 @@ class HumanDetectorDesktopApp(QMainWindow):
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
         p = convert_to_Qt_format.scaled(1280, 720, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
-
-
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
